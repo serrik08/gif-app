6bbb810276b7a43a17c2d573c5a64d2cc4a911c7
Sergio Geronimo <srgr89@gmail.com>
2023-11-23 08:45:10 -0400

    [feat] get gifs with seraching categories

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
 export const GifApp = () => {
-    return (
-        <>
-            <h1>GifApp</h1>
-        </>
+
+   const [categories, setCategories] = useState([ 'One Punch','Dragon Ball' ]);
+
+   const onAddCategory = ( newCategory ) => {
+      // const newCategory = event.target.value;
+      if( categories.includes(newCategory) ) return;
+      setCategories([ newCategory, ...categories ]);
+   };
+
+   return (
+      <>
+         <h1>GifApp</h1>
+
+         <AddCategory 
+            onNewCategory={ (event) => onAddCategory(event) }
+         />
+
+         
+         { 
+            categories.map( ( category ) => (
+               <GifGrid 
+                  key={ category } 
+                  category={ category }/>
+            ))
+         }
+         
+      </>
 )};
